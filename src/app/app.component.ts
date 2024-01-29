6b1e691b59bc25ff6a87d89a774fa0b444fc4910
TheTzunder30 <leocedsan@campus.monlau.com>
2024-01-29 19:23:11 +0100

    a

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,16 +1,18 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './componets/header/header.component';
+import { FooterComponent } from './componets/footer/footer.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent],
+  imports: [RouterOutlet, HeaderComponent, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title:string = 'test';
 
-  fruta:string = 'manzana';
+  frutas:string[] = ['manzana', 'pera', 'platano'];
+
 }
